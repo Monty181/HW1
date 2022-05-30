a4ea0d52a1b65ce3caca08435343f644a2cde9c6
Natalia Gushchina <natali.gushchina@gmail.com>
2022-05-30 17:31:01 +0300

    3

--- HW3/HW3__tast_warhips.py
+++ HW3/HW3__tast_warhips.py
@@ -9,14 +9,14 @@ class GameBoard:
 
     def __init__(self):
         for i in range(5):
-            GameBoard.game_field.append(["     "] * 5)
+            self.game_field.append(["     "] * 5)
         self.ship = (random.randint(1, 5), random.randint(1, 5))
 
     def print_game_field(self):
         border = "+-----+-----+-----+-----+-----+"
         for i in range(5):
             print(border)
-            field_str = "|".join(GameBoard.game_field[i])
+            field_str = "|".join(self.game_field[i])
             print(f"|{field_str}|")
         print(border)
 
@@ -24,14 +24,14 @@ class GameBoard:
         return self.ship
 
     def set_shot(self, a, b):
-        GameBoard.game_field[a-1][b-1] = "X".center(5)
+        self.game_field[a-1][b-1] = "X".center(5)
 
     def set_win_shot(self, a, b):
-        GameBoard.game_field[a-1][b-1] = "WIN".center(5)
+        self.game_field[a-1][b-1] = "WIN".center(5)
 
 
 Game = GameBoard()
-print(Game.ship)
+# print(Game.ship)
 Game.print_game_field()
 
 x, y = -1, -1
