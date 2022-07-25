c4f702ad122ab63a5420966d168e4ef2acf7887f
Natalia Gushchina <natali.gushchina@gmail.com>
2022-07-25 19:41:25 +0300

    fix

--- HW6/main.py
+++ HW6/main.py
@@ -1,7 +1,9 @@
-import app
+import censor_analyzer_app
 from file_manager import FileManager
 import sys
 import parser_module
+import os
+from pathlib import Path
 
 
 def main():
@@ -12,18 +14,26 @@ def main():
     if "-pt" in sys.argv:
         path_text = args.path_text
     else:
-        path_text = str(input("Hello, please enter path for the file with text: "))
+        path_text = ' '
+        while os.path.exists(path_text) is False:
+            while os.path.isfile(path_text) is False:
+                path_text = str(input("Hello, please enter path for the file with text: "))
     if "-bw" in sys.argv:
         path_bad_words = args.path_bad_words
     else:
-        path_bad_words = str(input("Please enter path for file with bad words: "))
+        path_bad_words = ' '
+        while os.path.exists(path_bad_words) is False:
+            while os.path.isfile(path_bad_words) is False:
+                path_bad_words = str(input("Please enter path for file with bad words: "))
     if "-cf" in sys.argv:
         path_censored_file = args.path_censored_file
     else:
-        path_censored_file = str(input("Please enter path you want to save censored file: "))
+        path_censored_file = ' '
+        while Path(path_censored_file).suffix != '.txt':
+            path_censored_file = str(input("Please enter path you want to save censored file: "))
 
-    censor_analyzer = app.CensorAnalyzer(FileManager.open_file(path_text), FileManager.open_file(path_bad_words))
-    FileManager.write_file(path_censored_file, censor_analyzer.remove_bad_words())
+    censor_analyzer = censor_analyzer_app.CensorAnalyzer(FileManager.open_file(path_text), FileManager.open_file(path_bad_words))
+    FileManager.write_file(path_censored_file, censor_analyzer.remove_count_bad_words()[0])
     censor_analyzer.print_console_count_bad_words()
 
 
